674bf36bd31c09e0780ce52a766c88d023119912
Kaylee Stevens <kayls@Kaylees-MBP.lan>
2021-12-03 01:46:22 -0600

    set api routes and pizza creation in mongo/goose

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
+
+//set up GET all and POST routes at /api/pizzas
+router
+    .route('/')
+    .get(getAllPizza)
+    .post(createPizza);
+
+//set up GET one, PUT, and DELETE at /api/pizza/:id 
+router
+    .route('/:id')
+    .get(getPizzaById)
+    .put(updatePizza)
+    .delete(deletePizza);
+
+module.exports = router;
